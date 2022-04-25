c29681d7ab4b7f12c5793bd179231bceedfac8d7
ablin2012 <adamlin.2009@gmail.com>
2022-04-25 14:31:33 -0700

    w2d1 classwork

--- w2d1_projects/Adam_Lin_bootcamp_project/lib/bootcamp.rb
+++ w2d1_projects/Adam_Lin_bootcamp_project/lib/bootcamp.rb
@@ -5,7 +5,7 @@ class Bootcamp
     @student_capacity = student_capacity
     @teachers = []
     @students = []
-    @grades = Hash.new([])
+    @grades = Hash.new {|h,k| h[k] = Array.new}
   end
 
   def name
@@ -22,5 +22,52 @@ class Bootcamp
 
   def students
     @students
+  end
+
+  def hire(str)
+    @teachers << str
+  end
+
+  def enroll(str)
+    if @students.length < @student_capacity
+      @students << str
+      return true
+    else
+      return false
+    end
+  end
+
+  def enrolled?(str)
+    return true if @students.include?(str)
+    return false
+  end
+
+  def student_to_teacher_ratio
+    return @students.length/@teachers.length
+  end
+
+  def add_grade(str, num)
+    if students.include?(str)
+      @grades[str] << num
+      return true
+    else
+      return false
+    end
+  end
+
+  def num_grades(str)
+    if !@students.include?(str)
+      return "This student does not exist"
+    else
+      return @grades[str].length
+    end
+  end
+
+  def average_grade(str)
+    if !@students.include?(str) || @grades[str].length == 0
+      return nil
+    else
+      return @grades[str].sum / @grades[str].length
+    end
   end
 end
--- w2d1_projects/Adam_Lin_monkey_patching_project/lib/array.rb
+++ w2d1_projects/Adam_Lin_monkey_patching_project/lib/array.rb
@@ -1,4 +1,72 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
+  def span
+    if self.length == 0
+      return nil
+    else
+      return self.max - self.min
+    end
+  end
+
+  def average
+    if self.length == 0
+      return nil
+    else
+      return self.sum.to_f/self.length
+    end
+  end
+
+  def median
+    if self.length == 0 
+      return nil
+    elsif self.length % 2 == 0
+      return (self.sort[self.length/2] + self.sort[self.length/2 - 1])/2.0
+    else
+      return self.sort[self.length/2]
+    end
+  end
+
+  def counts
+    count = Hash.new(0)
+    self.each do |ele|
+      count[ele] += 1
+    end
+    return count
+  end
+
+  def my_count(el)
+    count = 0
+    self.each do |ele|
+      count += 1 if ele == el
+    end
+    return count
+  end
+
+  def my_index(el)
+    self.each_with_index do |ele, idx|
+      return idx if ele == el
+    end
+    return nil
+  end
+
+  def my_uniq
+    unique = []
+    self.each do |ele|
+      unique << ele if !unique.include?(ele)
+    end
+    return unique
+  end
   
+  def my_transpose
+    trans = []
+    self.length.times {trans << []}
+    self.each do |subs|
+      i = 0
+      while i < subs.length
+        trans[i] << subs[i]
+        i += 1
+      end
+    end
+    return trans
+  end
 end
