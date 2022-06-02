0235eb0db96bb6970fdd8e0bdb64d3d77d821ae6
ablin2012 <adamlin.2009@gmail.com>
2022-06-01 19:47:48 -0700

    w7d4 project repo

---
+++ w7d3_projects/music_app/app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logout!
+        current_user.reset_session_token! if logged_in?
+        @current_user = nil
+        session[:session_token] = nil
+    end
+end
