bdf93df606f08a00bf367de2d0256570f8602341
ablin2012 <adamlin.2009@gmail.com>
2022-08-11 14:10:58 -0700

    worked on linked lists

--- leetcode/amazon_leetcode/easy/palindrome_linked_list.js
+++ leetcode/amazon_leetcode/easy/palindrome_linked_list.js
@@ -14,3 +14,44 @@
 
 // Follow up: Could you do it in O(n) time and O(1) space?
 
+const isPalindrome = function(head) { // method 1
+    let nums = [];
+    while (head) { // we create an array of the head values
+        nums.push(head.val);
+        head = head.next;
+    }
+    for (let i = 0; i <= nums.length/2; i++) { // then we iterate through the created array 
+        if (nums[i] != nums[nums.length - 1 - i]) { // check if the values at mirrored ends are the same
+            return false; // if they're not then we return false
+        }
+    }
+    return true; // if they are (ie. we reach the end of our loop) we return true
+}
+
+const isPalindrome2 = function(head) { // method 2
+    let fast = head;
+    let slow = head;
+    while (fast && fast.next) { // this will be used to find the middle node, which will be stored in variable 'slow'
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    let prev = null;
+    let temp;
+    while (slow) { // this will reverse the second half of the linked list
+        temp = slow.next;
+        slow.next = prev
+        prev = slow;
+        slow = temp;
+    }
+
+    let left = head; // we initialize our left to be the start of the array
+    let right = prev; // we initialize our right to be the start of the reversed array
+    while (right) {
+        if (left.val != right.val) { // if the values are equal then it isn't a palindrome
+            return false;
+        }
+        right = right.next;
+        left = left.next;
+    }
+    return true; // if all the values are equal and we reach the end then we know it's a palindrome
+}
---
+++ leetcode/amazon_leetcode/easy/remove_linked_list_elements.js
@@ -0,0 +1,35 @@
+// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+// Example 1:
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+
+// Example 2:
+// Input: head = [], val = 1
+// Output: []
+
+// Example 3:
+// Input: head = [7,7,7,7], val = 7
+// Output: []
+
+// Constraints:
+// The number of nodes in the list is in the range [0, 104].
+// 1 <= Node.val <= 50
+// 0 <= val <= 50
+
+const removeElements = function(head, val) {
+    let dummy = ListNode({next: head}); // we will create a dummy node that points to the head, so that we can easily find our head
+    let prev = dummy; // we will track a prev node
+    let curr = head; // and a current node
+    let next;
+    while (curr) {
+        next = curr.next;
+        if (curr.val === val) { // if the value of our current node is the val we are deleting
+            prev.next = next; // we change where our prev node is pointing to be the node after the deleted one
+        } else {
+            prev = curr; // if we didn't delete a node, we shift over our previous node
+        }
+        curr = curr.next; // we will always shift our current node
+    }
+    return dummy.next; // at the end we return the head of our newly adjusted linked list
+}
