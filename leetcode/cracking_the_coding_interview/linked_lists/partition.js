00b6dc632b5a3326b2a650f48cce26f8b98bdf25
ablin2012 <adamlin.2009@gmail.com>
2022-09-02 14:22:34 -0700

    partition linked list

---
+++ leetcode/cracking_the_coding_interview/linked_lists/partition.js
@@ -0,0 +1,24 @@
+const partition = function(head, p) {
+    let smaller = null;
+    let larger = null;
+    let sHead = null;
+    let lHead = null;
+    sHead.next = smaller;
+    lHead.next = larger;
+    let curr = head;
+    while (curr) {
+        if (curr.val < p) {
+            smaller.next = curr;
+            smaller = smaller.next;
+        } else {
+            larger.next = curr;
+            larger = larger.next;
+        }
+    }
+    smaller.next = lHead.next.next;
+    return sHead.next.next;
+}
+
+const partition2 = function(head, p) {
+    
+}
