969574f8d9fd60e2064f3d7c36409e037a103f1d
ablin2012 <adamlin.2009@gmail.com>
2022-09-06 15:08:16 -0700

    updated hackerrank answer

--- leetcode/hackerrank_coding_challenges/choose_flask.js
+++ leetcode/hackerrank_coding_challenges/choose_flask.js
@@ -31,3 +31,46 @@ function chooseFlask(requirements, flaskTypes, markings) {
         if (markings[i][0] === ans) return i;
     }
 }
+
+function chooseFlask(requirements, flaskTypes, markings) {
+    // Write your code here
+    let flasks = {};
+    let waste = {};
+    for (let i = 0; i < flaskTypes; i++) {
+        flasks[i] = [];
+        waste[i] = 0;
+    }
+    for (let i = 0; i < markings.length; i++) {
+        flasks[markings[i][0]].push(markings[i][1]);
+    }
+    for (let i = 0; i < flaskTypes; i++) {
+        let curr = flasks[i];
+        for (let j = 0; j < requirements.length; j++) {
+            let valid = false;
+            for (let k = 0; k < curr.length; k++) {
+                if (requirements[j] <= curr[k]) {
+                    waste[i] = waste[i] + curr[k] - requirements[j];
+                    valid = true;
+                    break
+                }
+            }
+            if (!valid) {
+                waste[i] = null;
+            }
+        }
+    }
+    let leastWaste = null;
+    let ans = -1;
+    for (let i = 0; i < flaskTypes; i++) {
+        if (waste[i] != null) {
+            if (leastWaste === null) {
+                leastWaste = waste[i];
+                ans = i;
+            } else if (waste[i] < leastWaste) {
+                leastWaste = waste[i];
+                ans = i;
+            }
+        }
+    }
+    return ans;
+}
