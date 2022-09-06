401d8949032ef01a60b4be37c2d00ac68e0a5afa
ablin2012 <adamlin.2009@gmail.com>
2022-09-06 15:58:34 -0700

    sum list reverse order

---
+++ leetcode/cracking_the_coding_interview/linked_lists/sum_list.js
@@ -0,0 +1,27 @@
+const sumList = function(node1, node2) {
+    let curr1 = node1;
+    let curr2 = node2;
+    let num1 = 0;
+    let num2 = 0;
+    let multiplier = 1;
+    while (curr1 != null || curr2 != null) {
+        num1 += curr1.val * multiplier;
+        num2 += curr2.val * multiplier;
+        multiplier *= 10;
+        curr1 = curr1.next;
+        curr2 = curr2.next;
+    }
+    let sum = num1 + num2;
+    let head = new Node;
+    head.val = sum % 10;
+    sum = Math.floor(sum/10);
+    let curr = head;
+    while (sum > 0) {
+        curr.next = new Node;
+        curr = curr.next;
+        curr.val = sum % 10;
+        sum = Math.floor(sum/10);
+    }
+    return head;
+}
+
