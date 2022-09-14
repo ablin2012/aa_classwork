fa31930f72482bef9d8b99b7acd86582fcd4d381
ablin2012 <adamlin.2009@gmail.com>
2022-09-14 16:59:38 -0700

    check balanced

--- leetcode/cracking_the_coding_interview/trees_and_graphs/check_balanced.js
+++ leetcode/cracking_the_coding_interview/trees_and_graphs/check_balanced.js
@@ -1,7 +1,14 @@
 const checkBalanced = function(root) {
     if (root === null) return true;
     if (root.left && root.right) {
-        checkBalanced(root.left);
-        
+        return (checkBalanced(root.left) && checkBalanced(root.right));
+    } else if (root.left) {
+        if (root.left.left || root.left.right) {
+            return false;
+        }
+    } else if (root.right) {
+        if (root.right.left || root.right.right) {
+            return false;
+        }
     }
 }
