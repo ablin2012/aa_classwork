39cd218c1a26d59a97971155130bac1e2e08e67e
ablin2012 <adamlin.2009@gmail.com>
2022-09-21 14:49:12 -0700

    linked list cycle

---
+++ leetcode/neetcode/linked_lists/linked_list_cycle.js
@@ -0,0 +1,11 @@
+var hasCycle = function(head) {
+    if (!head) return false;
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) return true;
+    }
+    return false;
+};
