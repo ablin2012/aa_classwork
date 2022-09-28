bb2799f3c210e81e53914a6d6cf1a53da6fb5a9e
ablin2012 <adamlin.2009@gmail.com>
2022-09-27 18:43:05 -0700

    added explanation

--- leetcode/neetcode/trees/merge_trees.js
+++ leetcode/neetcode/trees/merge_trees.js
@@ -1,10 +1,10 @@
 var mergeTrees = function(root1, root2) {
-    if (!root1 && !root2) return null;
-    let val1 = (!root1) ? 0 : root1.val;
-    let val2 = (!root2) ? 0 : root2.val;
-    const sum = val1 + val2;
-    let newNode = new TreeNode(sum);
-    newNode.left = mergeTrees((root1) ? root1.left : null, (root2) ? root2.left : null);
-    newNode.right = mergeTrees((root1) ? root1.right : null, (root2) ? root2.right : null);
-    return newNode;  
+    if (!root1 && !root2) return null; // our base case is when both of our roots our null and therefore the resulting merged node will be null
+    let val1 = (!root1) ? 0 : root1.val; // we grab the value from root1 if it exists else we default to 0
+    let val2 = (!root2) ? 0 : root2.val; // same with root2
+    const sum = val1 + val2; // we get the sum of our two values
+    let newNode = new TreeNode(sum); // and then create a new node with that sum as the value
+    newNode.left = mergeTrees((root1) ? root1.left : null, (root2) ? root2.left : null); // then we set the left to a recursive call on the left
+    newNode.right = mergeTrees((root1) ? root1.right : null, (root2) ? root2.right : null); // and set the right to a recursive call on the right
+    return newNode;  // we return the resulting new node
 };
