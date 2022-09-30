395bceaa68c0103540d7d314626b81f81b0e4294
ablin2012 <adamlin.2009@gmail.com>
2022-09-29 17:22:53 -0700

    lowest common ancestor

--- leetcode/neetcode/trees/lowest_common_ancestor.js
+++ leetcode/neetcode/trees/lowest_common_ancestor.js
@@ -4,3 +4,13 @@ var lowestCommonAncestor = function(root, p, q) {
     if (p.val > root.val) return lowestCommonAncestor(root.right, p, q);
     return lowestCommonAncestor(root.left, p, q);
 };
+
+const lowestAncestor = function(root, p, q){
+	if(p.val < root.val && q.val < root.val) {
+	    return lowestAncestor(root.left, p, q);
+    }else if (p.val > root.val && q.val > root.val) {
+        return lowestAncestor(root.right, p, q);
+    }else{
+	    return root;
+    };
+};
