d3cc71e344f0c8a2994d0457f9e0d6b83e8a270e
ablin2012 <adamlin.2009@gmail.com>
2022-10-03 20:55:29 -0700

    leetcode

--- leetcode/neetcode/trees/balanced_binary_tree.js
+++ leetcode/neetcode/trees/balanced_binary_tree.js
@@ -6,3 +6,18 @@ var isBalanced = function(root) {
     return (isBalanced(root.right) && isBalanced(root.left));
 };
 
+var subTreeLength = function(root) {
+    if (!root) return 0;
+    let height = 0;
+    let queue = [root];
+    while(queue.length) {
+        height += 1;
+        let prev = queue;
+        queue = [];
+        for (let i = 0; i < prev.length; i++) {
+            if (prev[i].left) queue.push(prev[i].left);
+            if (prev[i].right) queue.push(prev[i].right);
+        }
+    }
+    return height;
+}
