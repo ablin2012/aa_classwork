17b93dba71d6c2170044b5323cf35705a4396a66
ablin2012 <adamlin.2009@gmail.com>
2022-10-06 12:39:23 -0700

    rotate image

---
+++ leetcode/neetcode/math_and_geom/rotate_image.js
@@ -0,0 +1,17 @@
+var rotate = function(matrix) {
+    if (matrix.length === 0 || matrix.length != matrix[0].length) return false;
+    let n = matrix.length;
+    for (let layer = 0; layer < Math.floor(n/2); layer++) {
+        let first = layer;
+        let last = n - layer - 1;
+        for (let i = first; i < last; i++) {
+            let offset = i - first;
+            let top = matrix[first][i];
+            matrix[first][i] = matrix[last - offset][first]; // left to top
+            matrix[last - offset][first] = matrix[last][last - offset]; // bottom to left
+            matrix[last][last - offset] = matrix[i][last]; // right to bottom
+            matrix[i][last] = top; // top to right
+        }
+    }
+    return matrix;
+};
